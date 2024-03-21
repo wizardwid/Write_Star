ee1f751661f70e0477e97c7cdec39fa41ef96dc7
wizardwid <s2335@e-mirim.hs.kr>
2024-03-22 08:18:22 +0900

    왼쪽 기본 삼각형

---
+++ src/OppositeRightTriangle.java
@@ -0,0 +1,14 @@
+public class OppositeRightTriangle {
+
+    public static void main(String[] args) {
+        for(int i=1; i<6; i++){
+            for(int j=5; j>i; j--){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i; k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
